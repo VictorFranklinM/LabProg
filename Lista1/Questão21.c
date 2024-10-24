d6e3e325e6886fb6b64b44da946986d01e208f6c
VictorFranklinM <victorfranklin2006@gmail.com>
2024-10-23 22:15:59 -0300

    Create Questão21.c

---
+++ Lista1/Questão21.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    printf("Inteiro equivalente a 'A': %d\n",'A');
+    printf("Inteiro equivalente a 'B': %d\n",'B');
+    printf("Inteiro equivalente a 'C': %d\n",'C');
+    printf("Inteiro equivalente a 'a': %d\n",'a');
+    printf("Inteiro equivalente a 'b': %d\n",'b');
+    printf("Inteiro equivalente a 'c': %d\n",'c');
+    printf("Inteiro equivalente a '0' (número): %d\n",0);
+    printf("Inteiro equivalente a '0' (símbolo): %d\n",'0');
+    printf("Inteiro equivalente a '12': %d\n",12);
+    printf("Inteiro equivalente a '$': %d\n",'$');
+    printf("Inteiro equivalente a '*': %d\n",'*');
+    printf("Inteiro equivalente a '+': %d\n",'+');
+    printf("Inteiro equivalente a '/': %d\n",'/');
+    printf("Inteiro equivalente a ' ': %d\n",' ');
+    return 0;
+}
