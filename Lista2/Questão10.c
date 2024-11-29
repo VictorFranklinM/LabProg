6cf7af927fb4e56bb844aba964ecd1360705dbbc
VictorFranklinM <victorfranklin2006@gmail.com>
2024-11-28 22:47:35 -0300

    Delete Lista2/Questão10.c

--- Lista2/Questão10.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-int main(){
-    int num;
-    for (num=15;num<=90;num++){
-        num%4 ? num : printf("%d é divisível por 4 e seu quadrado é: %d\n",num,(int)pow(num,2));
-    }
-    return 0;
-}
