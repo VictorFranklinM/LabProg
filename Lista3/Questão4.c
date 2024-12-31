e0e325a663fd2450b57d4da49be026e76e57924b
VictorFranklinM <victorfranklin2006@gmail.com>
2024-12-30 23:07:01 -0300

    Questão4
    Feita sem strcmp.

---
+++ Lista3/Questão4.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAM 20
+
+char *alocarmem();
+char getstr(char *str);
+void checkigual(char *s1, char *s2);
+
+int main(){
+    char *str1 = alocarmem();
+    char *str2 = alocarmem();
+
+    getstr(str1);
+    getstr(str2);
+
+    checkigual(str1,str2);
+
+    free(str1);
+    free(str2);
+
+    return 0;
+}
+
+char *alocarmem(){
+    char *str = NULL;
+    if(!(str = (char*)malloc(TAM*sizeof(char)))){
+        puts("Memória indisponível.");
+        exit(1);
+    }
+    return str;
+}
+
+char getstr(char *str){
+    printf("Digite uma string com até %d caracteres: ",TAM-1);
+    fgets(str,TAM,stdin);
+    str[strcspn(str,"\n")] = 0;
+    return *str;
+}
+
+void checkigual(char *s1, char *s2){
+    int igual = 1;
+
+    for(int i = 0; (*(s1+i) != '\0')||(*(s2+i) != '\0'); i++){
+        if((*(s1+i))!=(*(s2+i))){
+            igual = 0;
+        }
+    }
+
+    igual ? puts("As strings são iguais.") : puts("As strings são diferentes.");
+}
